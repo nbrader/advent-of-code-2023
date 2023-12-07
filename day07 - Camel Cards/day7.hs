0c3abdcbab37d8c3f67a92568311841648252a41
nbrader <nbrader@gmail.com>
2023-12-07 18:32:12 +0000

    Completed day 7 (although I think it could be more efficient if I cached the best joker-hand map)

--- day07 - Camel Cards/day7.hs
+++ day07 - Camel Cards/day7.hs
@@ -15,10 +15,10 @@
 -- Output --
 ------------
 -- *Main> day7part1
--- 
+-- 251216224
 
 -- *Main> day7part2
--- 
+-- 250825971
 
 
 -------------
@@ -63,6 +63,22 @@ cardValue 'Q' = 11
 cardValue 'K' = 12
 cardValue 'A' = 13
 
+cardWithJokersValue 'J' = 1
+cardWithJokersValue '2' = 2
+cardWithJokersValue '3' = 3
+cardWithJokersValue '4' = 4
+cardWithJokersValue '5' = 5
+cardWithJokersValue '6' = 6
+cardWithJokersValue '7' = 7
+cardWithJokersValue '8' = 8
+cardWithJokersValue '9' = 9
+cardWithJokersValue 'T' = 10
+cardWithJokersValue 'Q' = 11
+cardWithJokersValue 'K' = 12
+cardWithJokersValue 'A' = 13
+
+allCards = "23456789TJQKA"
+
 handType :: Hand -> Type
 handType hand = if 5 `elem` numbers
                   then FiveOfAKind
@@ -74,21 +90,37 @@ handType hand = if 5 `elem` numbers
                                     then ThreeOfAKind
                                     else if (== 2) . length $ filter (== 2) numbers
                                           then TwoPair
-                                          else if 3 `elem` numbers
+                                          else if 2 `elem` numbers
                                                 then OnePair
                                                 else HighCard
   where numbers = map length . group . sort . handCards $ hand
 
+newtype HandWithJokers = HandWithJokers {fromHandWithJokers :: Hand} deriving (Show, Eq)
+
+handWithJokersCards = handCards . fromHandWithJokers
+handWithJokersBid = handBid . fromHandWithJokers
+
+bestHandWithJokersHandType :: HandWithJokers -> Type
+bestHandWithJokersHandType (HandWithJokers h) = maximum $ map handType hands
+  where hands = map (\cs -> Hand cs (handBid h)) $ sequence [if c == 'J' then allCards else [c] | c <- handCards h]
+
 instance Ord Hand where
   compare x y = comparing handType x y <> comparing (map cardValue . handCards) x y
+  
+instance Ord HandWithJokers where
+  compare x y = comparing bestHandWithJokersHandType x y <> comparing (map cardWithJokersValue . handWithJokersCards) x y
+-- I might have to avoid using Ord to allow me to cache the map from joker hand to best hand
 
 readHand :: String -> Hand
 readHand inStr = (\[cards, bid] -> Hand cards (read bid)) $ words inStr
 
+readHandWithJokers :: String -> HandWithJokers
+readHandWithJokers = HandWithJokers . readHand
+
 day7part1 = do
-  contents <- readFile "day7 (data).csv"
+  contents <- readFile "day7 (data 3).csv"
   print $ sum . map (\(i,h) -> i * handBid h) . zip [1..] . sort . map readHand . lines $ contents
 
--- day7part2 = do
-  -- contents <- readFile "day7 (data).csv"
-  -- print $ numberOfWinningOptions' $ readAsSingleRace $ contents
+day7part2 = do
+  contents <- readFile "day7 (data 3).csv"
+  print $ sum . map (\(i,h) -> i * handWithJokersBid h) . zip [1..] . sort . map readHandWithJokers . lines $ contents
