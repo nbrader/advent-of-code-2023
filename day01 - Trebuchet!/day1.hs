69a04f4ce55b6d98feebbbcdabbc919965644191
nbrader <nbrader+github@gmail.com>
2023-12-10 00:10:04 +0000

    Prettifying formatting

--- day01 - Trebuchet!/day1.hs
+++ day01 - Trebuchet!/day1.hs
@@ -38,16 +38,27 @@ main = day1part2
 
 day1part1 = do
   contents <- readFile "day1 (data).csv"
-  let total = sum . map read . map (\xs -> [head xs, last xs]) . map (filter isDigit) . lines $ contents
+  let total = sum . map read . map (\xs -> [head xs, last xs])
+                  . map (filter isDigit) . lines $ contents
   print $ total
 
 digitWords :: M.Map String Int
-digitWords = M.fromList [("0",0), ("1",1), ("2",2), ("3",3), ("4",4), ("5",5), ("6",6), ("7",7), ("8",8),("9",9), ("zero",0), ("one",1), ("two",2), ("three",3), ("four",4), ("five",5), ("six",6), ("seven",7), ("eight",8), ("nine",9)]
+digitWords
+    = M.fromList [
+        ("0",0), ("1",1), ("2",2), ("3",3), ("4",4),
+        ("5",5), ("6",6), ("7",7), ("8",8),("9",9),
+        ("zero",0), ("one",1), ("two",2), ("three",3), ("four",4),
+        ("five",5), ("six",6), ("seven",7), ("eight",8), ("nine",9)
+        ]
 
 lineToNum :: String -> Int
 lineToNum xs = read . concatDigits . firstAndLast $ allDigitsInLine
   where allDigitsInLine :: [Int]
-        allDigitsInLine = [fromJust (M.lookup key digitWords) | end <- tails xs, key <- M.keys digitWords, key `isPrefixOf` end]
+        allDigitsInLine
+            = [fromJust (M.lookup key digitWords) |
+                end <- tails xs,
+                key <- M.keys digitWords,
+                key `isPrefixOf` end]
         
         firstAndLast :: [a] -> [a]
         firstAndLast xs = [head xs, last xs]
