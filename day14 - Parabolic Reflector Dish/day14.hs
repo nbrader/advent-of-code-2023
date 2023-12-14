aa612613fcd9029e3beb299f2c0e70a670ce00f4
nbrader <nbrader@gmail.com>
2023-12-14 19:41:45 +0000

    Committing naive solution to part 2 which doesn't seem to end quickly (I've not seen it end after a few minutes even on the simplest of inputs).

---
+++ day14 - Parabolic Reflector Dish/day14.hs
@@ -0,0 +1,59 @@
+#!/usr/bin/env stack
+-- stack --resolver lts-21.22 ghci --package containers-0.6.7 --package linear-1.22 --package split-0.2.3.5 --package parallel-3.2.2.0
+
+---------------------------------------------
+---------------------------------------------
+----  Day 14:  Parabolic Reflector Dish  ----
+---------------------------------------------
+---------------------------------------------
+{-
+    To build, run the following shell command in this directory:
+        stack --resolver lts-21.22 ghc --package containers-0.6.7 --package linear-1.22 -- -threaded -O2 '.\day14.hs'
+-}
+
+------------
+-- Output --
+------------
+-- *Main> day14part1
+-- 
+
+-- *Main> day14part2
+-- 
+
+
+-------------
+-- Imports --
+-------------
+import Data.List (foldl', nub, sort, transpose, isPrefixOf, findIndex, findIndices, intercalate, reverse, partition, intersperse)
+import Data.List.Split (splitOn, chunksOf)
+import qualified Data.Map as M
+import Data.Maybe (fromJust, maybeToList, catMaybes)
+import Linear.V2
+import Control.Monad (guard)
+import Debug.Trace (trace)
+import Control.Parallel.Strategies
+import Data.Bits
+
+
+-------------
+-- Program --
+-------------
+main = day14part2
+
+readColumns :: String -> [String]
+readColumns = transpose . lines
+
+roll = map (intercalate "#" . map ((\(rocks,spaces) -> rocks ++ spaces) . partition (== 'O'))) . map (splitOn "#")
+load = sum . map sum . map (zipWith (\i c -> i * if c == 'O' then 1 else 0) [1..]) . map reverse
+rotateRocks = map reverse . transpose
+
+day14part1 = do
+  contents <- readFile "day14 (data).csv"
+  let cols = readColumns $ contents
+  print . load . roll $ cols
+
+day14part2 = do
+  contents <- readFile "day14 (example).csv"
+  let cols = readColumns $ contents
+  -- print . load . foldr (flip (.)) id (intersperse rotateRocks (concat $ replicate 1000000000 (replicate 4 roll))) $ cols
+  print . load $ foldl' (flip ($!)) cols (intersperse rotateRocks (concat $ replicate 1000000000 (replicate 4 roll)))
