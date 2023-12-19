fdcb926808ea184c249bca78307ba9317ef4f923
nbrader <nbrader+github@gmail.com>
2023-12-19 01:11:16 +0000

    Committing as far as I got with improving day18

--- day18 - Lavaduct Lagoon/day18.hs
+++ day18 - Lavaduct Lagoon/day18.hs
@@ -24,25 +24,34 @@
 -------------
 -- Imports --
 -------------
-import Data.List (foldl', nub, sort)
+import Data.List (foldl', nub, sort, sortBy, groupBy, delete)
 import qualified Data.Map as M
-import Data.Maybe (fromJust, maybeToList)
+import Data.Maybe (fromJust, maybeToList, catMaybes)
 import Linear hiding (trace)
 import Linear.V2
 import Debug.Trace (trace)
+import Data.Ord
+import Data.Function
+import Data.Tuple
 
 
 -------------
 -- Program --
 -------------
-main = day18part2
+main = day18part1
 
-data DigLine = DigLine {digLineDir :: V2 Int, digLineLength :: Int} deriving (Show)
+data Instruction = Instruction {instructionDir :: V2 Int, instructionLength :: Int} deriving (Show)
 
 vUp = V2 ( 0) (-1)
 vDn = V2 ( 0) ( 1)
 vLt = V2 (-1) ( 0)
 vRt = V2 ( 1) ( 0)
+
+getX (V2 x y) = x
+getY (V2 x y) = y
+
+rot90 :: V2 Int -> V2 Int
+rot90 (V2 x y) = V2 (-y) x
 
 readDirStr :: String -> V2 Int
 readDirStr "U" = vUp
@@ -69,47 +78,137 @@ charToHexDigit digit = read [digit]
 readHex :: String -> Int
 readHex = sum . zipWith (\x y -> y^x) [0..] . map charToHexDigit
 
-readDigLines :: String -> [DigLine]
-readDigLines inStr = do
+readInstructions :: String -> [Instruction]
+readInstructions inStr = do
     row <- rows
     let [dirStr, lenStr, hexStr] = words row
     let dir = readDirStr dirStr
     let len = read lenStr
-    return $ DigLine dir len
+    return $ Instruction dir len
   where rows = lines inStr
 
-readDigLines2 :: String -> [DigLine]
-readDigLines2 inStr = do
+readInstructions2 :: String -> [Instruction]
+readInstructions2 inStr = do
     row <- rows
     let [dirStr, lenStr, hexStr] = words row
     let len = readHex . tail . tail . init $ hexStr
     let dir = readDirDigit . last . init $ hexStr
-    return $ DigLine dir len
+    return $ Instruction dir len
   where rows = lines inStr
 
 start = V2 0 0
 
-pathFromStart :: [DigLine] -> [V2 Int]
+pathFromStart :: [Instruction] -> [V2 Int]
 pathFromStart = foldl' step [start]
-  where step (v:visited) (DigLine dir len) = [v + i*^dir | i <- [len, (len-1) .. 1]] ++ v:visited
+  where step (v:visited) (Instruction dir len) = (v + len*^dir):v:visited
+
+type LinearPath = (V2 Int, V2 Int)
+
+-- toLinearPaths :: [V2 Int] -> [LinearPath]
+-- toLinearPaths path = zip (init path) (tail path)
+
+toLinearPaths :: [V2 Int] -> [LinearPath]
+toLinearPaths path = catMaybes $ map (\linearPath -> if horizontal linearPath then trimHorizontalEnds linearPath else Just linearPath) $ zip (init path) (tail path)
+
+trimHorizontalEnds :: LinearPath -> Maybe LinearPath
+trimHorizontalEnds linearPath@(V2 startX startY, V2 endX endY)
+    | xDisp == 1 = Nothing
+    | otherwise  = Just (V2 (startX+dir) startY, V2 (endX-dir) endY)
+  where xDisp = getLinearPathXDisp linearPath
+        dir = signum $ xDisp
+
+horizontal :: LinearPath -> Bool
+horizontal (start, end) = getY start == getY end
+
+vertical :: LinearPath -> Bool
+vertical (start, end) = getX start == getX end
+
+type Interval = (Int,Int)
+projectToX :: LinearPath -> Interval
+projectToX ((V2 startX _),(V2 endX _)) = (startX, endX)
+
+toIncreasing :: Interval -> Interval
+toIncreasing (start,end) = (min start end, max start end)
+
+getLinearPathXDisp (start,end) = getX start - getX end
+getLinearPathYDisp (start,end) = getX start - getX end
+
+getLinearPathXSize = abs . getLinearPathXDisp
+getLinearPathYSize = abs . getLinearPathYDisp
+
+-- linearPathsToInternalArea :: [LinearPath] -> Int
+linearPathsToInternalArea linearPaths = pathGroups --sum groupAreas
+  where verticalPaths = filter vertical linearPaths
+        sortedVerticals@(leftMostVerticalPath:_) = sortBy (comparing (getX . fst)) $ verticalPaths
+        (leftMostStart, leftMostEnd) = leftMostVerticalPath
+        isClockwise = getY leftMostEnd - getY leftMostStart < 0
+        refinedPaths = pathsPartitionHoriz linearPaths
+        pathGroups = groupBy ((==) `on` (toIncreasing . projectToX)) . sortBy (comparing (toIncreasing . projectToX)) $ refinedPaths
+        groupAreas = map area pathGroups
+        area pathGroup
+            | width == 0 = undefined $ pathGroup
+          where firstLine = head pathGroup
+                width = getLinearPathXSize firstLine
+
+pathsPartitionHoriz :: [LinearPath] -> [LinearPath]
+pathsPartitionHoriz linearPaths = do
+    originalPath <- linearPaths
+    refineLinearPathByPaths originalPath (delete originalPath linearPaths)
+
+refineLinearPathByPaths :: LinearPath -> [LinearPath] -> [LinearPath]
+refineLinearPathByPaths originalPath linearPaths = foldl' (\partitionOfOriginal other -> concat $ map (refineLinearPathByLinearPath other) partitionOfOriginal) [originalPath] linearPaths
+
+makeRightward :: LinearPath -> LinearPath
+makeRightward linearPath
+    | isRightward linearPath = flipLinearPath linearPath
+    | otherwise              =                linearPath
+
+isRightward :: LinearPath -> Bool
+isRightward (start,end) = getX end >= getX start
+
+flipLinearPath :: LinearPath -> LinearPath
+flipLinearPath (start,end) = (end,start)
+
+refineLinearPathByLinearPath :: LinearPath -> LinearPath -> [LinearPath]
+refineLinearPathByLinearPath applied original
+    | isRightward original =                      refineRightwardLinearPathByRightwardLinearPath rightwardApplied original
+    | otherwise            = map flipLinearPath $ refineRightwardLinearPathByRightwardLinearPath rightwardApplied (makeRightward original)
+  where rightwardApplied = makeRightward applied
+
+refineRightwardLinearPathByRightwardLinearPath :: LinearPath -> LinearPath -> [LinearPath]
+refineRightwardLinearPathByRightwardLinearPath (appliedStart, appliedEnd) (originalStart, originalEnd)
+    | originalEndX   <  appliedStartX || originalStartX >  appliedEndX = [(originalStart, originalEnd)]
+    | originalStartX <  appliedStartX && originalEndX   <= appliedEndX = [(originalStart, appliedStartSubOneCut), (appliedStartCut, originalEnd)]
+    | originalStartX <  appliedStartX && originalEndX   >  appliedEndX = [(originalStart, appliedStartSubOneCut), (appliedStartCut, appliedEndCut), (appliedEndPlusOneCut, originalEnd)]
+    | originalStartX == appliedStartX && originalEndX   <= appliedEndX = [(appliedStartCut, originalEnd)]
+    | originalStartX == appliedStartX && originalEndX   >  appliedEndX = [(appliedStartCut, appliedEndCut), (appliedEndPlusOneCut, originalEnd)]
+    | originalStartX >  appliedStartX && originalEndX   <= appliedEndX = [(originalStart, originalEnd)]
+    | otherwise                                                        = [(originalStart, appliedEndCut), (appliedEndPlusOneCut, originalEnd)]
+  where y = getY originalStart
+        originalStartX = getX originalStart
+        originalEndX   = getX originalEnd
+        appliedStartX  = getX appliedStart
+        appliedEndX    = getX appliedEnd
+        appliedStartSubOneCut = V2 (appliedStartX-1) y
+        appliedStartCut       = V2  appliedStartX    y
+        appliedEndCut         = V2  appliedEndX      y
+        appliedEndPlusOneCut  = V2 (appliedEndX+1)   y
+
+-- refineLinearPathByPaths originalPath linearPaths = foldr 
+
+-- refineLinearPathByPath originalPath linearPaths = foldr []
 
 day18part1 = do
-  contents <- readFile "day18 (data).csv"
-  let edgesPositions = drop 1 . pathFromStart . readDigLines $ contents
-  let interior = findInterior $ edgesPositions
-  print (length edgesPositions + length interior)
-
-day18part2 = do
   contents <- readFile "day18 (example).csv"
-  let edgesPositions = drop 1 . pathFromStart . readDigLines2 $ contents
-  let interior = findInterior $ edgesPositions
-  print (length edgesPositions + length interior)
-
-rot90 :: V2 Int -> V2 Int
-rot90 (V2 x y) = V2 (-y) x
-
-getX (V2 x y) = x
-getY (V2 x y) = y
+  let instructions = readInstructions2 contents
+  let linearPaths = toLinearPaths . pathFromStart $ instructions
+  -- mapM_ print linearPaths
+  -- putStrLn ""
+  mapM_ print $ linearPathsToInternalArea linearPaths
+  -- putStrLn ""
+  -- mapM_ print $ map (map projectToX) $ linearPathsToInternalArea linearPaths
+  -- putStrLn ""
+  -- mapM_ print $ map (map (toIncreasing . projectToX)) $ linearPathsToInternalArea linearPaths
 
 inBounds :: V2 Int -> (V2 Int, V2 Int) -> Bool
 inBounds (V2 x y) (V2 minX minY, V2 maxX maxY) = x >= minX && x < maxX && y >= minY && y < maxY
