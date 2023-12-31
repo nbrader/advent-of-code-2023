1492c5ed064769849147a0b419e2ca4ee1039472
nbrader <nbrader+github@gmail.com>
2023-12-31 14:19:17 +0000

    Made reuse of existing implementations more obvious

--- day21 - Step Counter/WalkableRepTilesWorld.hs
+++ day21 - Step Counter/WalkableRepTilesWorld.hs
@@ -50,23 +50,16 @@ import WalkableWorld as Class
 data WalkableRepTilesWorld = WalkableRepTilesWorld {coreWorld :: World}
 
 instance WalkableWorld WalkableRepTilesWorld where
-    readWorld :: String -> (Int,WalkableRepTilesWorld)
-    readWorld = fmap fromBounded . Class.readWorld
-
-    showWorld :: Int -> WalkableRepTilesWorld -> String
-    showWorld height world = Class.showWorld height (toBounded world)
-
-    removeForbidden :: WalkableRepTilesWorld -> WalkableRepTilesWorld
-    removeForbidden = fromBounded . Class.removeForbidden . toBounded
+    readWorld        = fmap fromBounded . Class.readWorld
+    showWorld height =                    Class.showWorld height . toBounded
+    removeForbidden  =      fromBounded . Class.removeForbidden  . toBounded
+    setOAtS          =      fromBounded . Class.setOAtS          . toBounded
+    coreWorld        =                    Class.coreWorld        . toBounded
 
     progressByAStep :: WalkableRepTilesWorld -> WalkableRepTilesWorld
     progressByAStep w = undefined --removeForbidden $ WalkableRepTilesWorld $ combineWorlds $ map (\dir -> moveLayerInWorld 'O' dir (Class.coreWorld w)) allDirs
 
-    setOAtS :: WalkableRepTilesWorld -> WalkableRepTilesWorld
-    setOAtS = fromBounded . Class.setOAtS . toBounded
 
-    coreWorld :: WalkableRepTilesWorld -> W.World
-    coreWorld = Class.coreWorld . toBounded
 
 fromBounded :: WalkableBoundedWorld -> WalkableRepTilesWorld
 fromBounded = WalkableRepTilesWorld . Class.coreWorld
