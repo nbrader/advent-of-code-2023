181926fcfaa2f98a374d2921642b3faa55d9ef3f
nbrader <nbrader+github@gmail.com>
2023-12-31 02:14:00 +0000

    Made something of a useful world data type

--- day21 - Step Counter/day21.hs
+++ day21 - Step Counter/day21.hs
@@ -1,5 +1,5 @@
 #!/usr/bin/env stack
--- stack --resolver lts-21.22 ghci --package containers-0.6.7 --package linear-1.22 --package split-0.2.3.5 --package deque-0.4.4.1
+-- stack --resolver lts-21.22 ghci --package containers-0.6.7 --package linear-1.22 --package split-0.2.3.5 --package deque-0.4.4.1 --package safe-0.3.19
 
 --------------------------------
 --------------------------------
@@ -24,11 +24,11 @@
 -------------
 -- Imports --
 -------------
-import Data.List (foldl', nub, sort, sortBy, groupBy, delete, find)
-import Data.List.Split (splitOn)
+import Data.List (foldl', nub, sort, sortBy, groupBy, delete, find, transpose)
+import Data.List.Split (splitOn, chunksOf)
 import qualified Data.Map as M
-import Data.Maybe (fromJust, maybeToList, catMaybes)
-import Linear hiding (trace)
+import Data.Maybe (fromJust, maybeToList, catMaybes, fromMaybe)
+import Linear hiding (trace, transpose)
 import Linear.V2
 import Debug.Trace (trace)
 import Data.Ord
@@ -36,10 +36,11 @@ import Data.Function
 import Data.Tuple
 import GHC.Exts as F
 import Data.Bits
-import Control.Monad (guard)
+import Control.Monad (guard, join)
 import Data.Monoid
 import Data.Foldable
 import Data.Ord
+import Safe (atMay)
 
 
 -------------
@@ -49,40 +50,145 @@ import Data.Ord
 
 
 -- Each obj has a shape of Points encoded as Integer.
-type SinglePoint = Integer
+type SingularPoint = (Int,Int)
 type Points = Integer
 
-data World = World {worldBG :: Char, worldLayers :: M.Map Char Points, worldPoints :: M.Map Char SinglePoint, worldWidth :: Int}
+data World
+    = World { worldBG :: Char
+            , worldLayers :: M.Map Char Points
+            , worldPoints :: M.Map Char SingularPoint
+            , worldWidth :: Int }
 
-readWorld :: String -> World
-readWorld = undefined
-  where readChar2Ds :: [Char] -> [((Int,Int), Char)]
+emptyWorld :: Char -> Int -> World
+emptyWorld bgChar width = World bgChar mempty mempty width
+
+
+-- Assumes all rows have equal length
+readWorld :: Char -> [Char] -> String -> (Int,World)
+readWorld bgChar singularChars inStr
+    = ( height
+      , World { worldBG = bgChar,
+                worldLayers = mempty,
+                worldPoints = singularPoints,
+                worldWidth = width } )
+       
+  where rows = lines inStr
+        height = length rows
+        width
+          | height == 0 = 0
+          | otherwise   = length $ head rows
+        char2Ds = concat $ map readChar2Ds rows
+        singularPoints = M.fromList . catMaybes
+                                    . map (\c -> find (\(c', (x,y)) -> c' == c) char2Ds)
+                                    $ singularChars
+        
+        readChar2Ds :: [Char] -> [(Char, (Int,Int))]
         readChar2Ds inStr = do
             let rows = lines inStr
             (y,row)  <- zip [0..] rows
             (x,char) <- zip [0..] row
             
-            return ((x, y), char)
+            guard $ char /= bgChar
+            
+            return (char, (x, y))
 
 showWorld :: Int -> (Char -> Char -> Ordering) -> World -> String
-showWorld height charZOrder world = undefined
+showWorld height charZOrder world = unlines . reverse . take height . chunksOf width . map (fromMaybe bgChar) $ listOfMaybeCharsFromLayersAndPoints
+  where (World bgChar layers points width) = world
+        listsOfMaybeCharsFromLayers = prioritize charZOrder $ M.mapWithKey (\c n -> layerToMaybeChars c n) layers
+        listOfMaybeCharsFromLayers = combineMaybeCharLists listsOfMaybeCharsFromLayers
+        charsAndPoints = map head . groupBy ((==) `on` snd) . sortBy (\(aChar,aPos) (bChar,bPos) -> compare aPos bPos <> compare aChar bChar) . M.toList $ points
+        charsAndIndices = map (fmap (pointToIndex width)) charsAndPoints
+        listOfMaybeCharsFromLayersAndPoints = foldr (\(c,i) acc -> let maybeOld = join (acc `atMay` i) in replace acc (i, Just $ minimum $ catMaybes [maybeOld, Just c])) listOfMaybeCharsFromLayers charsAndIndices
+        
+        combineMaybeCharLists :: [[Maybe a]] -> [Maybe a]
+        combineMaybeCharLists = map (getFirst . fold . map First) . transpose
+        
+        prioritize :: Ord a1 => (a1 -> a1 -> Ordering) -> M.Map a1 a2 -> [a2]
+        prioritize charZOrder = (\m -> map (fromJust . flip M.lookup m) (sortBy charZOrder $ M.keys m))
+        
+        layerToMaybeChars :: Char -> Integer -> [Maybe Char]
+        layerToMaybeChars c n = map (\i -> if n `testBit` i then Just c else Nothing) [0..]
+
+replace [] _ = []
+replace (_:xs) (0,a) = a:xs
+replace (x:xs) (n,a) =
+  if n < 0
+    then (x:xs)
+    else x: replace xs (n-1,a)
+
+printWorld :: Int -> (Char -> Char -> Ordering) -> World -> IO ()
+printWorld height charZOrder world = putStrLn $ showWorld height charZOrder world
+
+
+-- Testing
+exampleWorld1 :: World
+exampleWorld1 = World '.' (M.fromList [('U',9),('Z',1)]) (M.fromList [('V',(1,0))]) 10
+
+exampleWorld2 :: World
+exampleWorld2 = World '.' (M.fromList [('U',20),('Z',1)]) (M.fromList [('V',(3,3))]) 10
+
+exampleWorld3 :: World
+exampleWorld3 = moveLayer 'U' (-1,-1) $ exampleWorld2
+
+exampleWorld4 :: World
+exampleWorld4 = exampleWorld1 `combineWorlds` exampleWorld2
+
+examplePrint1 = printWorld 10 (comparing id) exampleWorld1
+examplePrint2 = printWorld 10 (comparing id) exampleWorld2
+examplePrint3 = printWorld 10 (comparing id) exampleWorld3
+examplePrint4 = printWorld 10 (comparing id) exampleWorld4
+
 
 -- Assumes worlds are same size
--- Keeps characters from the first where they coincide
+-- Left-biased such that the background character and any singular points they share are taken from the left
 combineWorlds :: World -> World -> World
 combineWorlds w1 w2
-    = w1 { worldLayers = M.unionWithKey combineLayers (worldLayers w1) (worldLayers w2),
-           worldPoints = M.unionWithKey combinePoints (worldPoints w1) (worldPoints w2) }
-  where combineLayers :: Char -> Points -> Points -> Points
-        combineLayers = undefined
+    = w1 { worldLayers = M.unionWith combineLayers (worldLayers w1) (worldLayers w2),
+           worldPoints = M.unionWith combinePoints (worldPoints w1) (worldPoints w2) }
+  where combineLayers :: Points -> Points -> Points
+        combineLayers points1 points2 = points1 .|. points2
         
-        combinePoints :: Char -> SinglePoint -> SinglePoint -> SinglePoint
-        combinePoints = undefined
+        combinePoints :: SingularPoint -> SingularPoint -> SingularPoint
+        combinePoints point1 _ = point1
 
-hasPoint :: Char -> (Int,Int) -> World -> Bool
-hasPoint = undefined
+hasPoint :: Char -> SingularPoint -> World -> Bool
+hasPoint char point world = inSingularPoints || inLayers
+  where
+    inSingularPoints = case M.lookup char (worldPoints world) of
+        Just p -> p == point
+        Nothing -> False
+
+    inLayers = case M.lookup char (worldLayers world) of
+        Just bits -> testBit bits (pointToIndex (worldWidth world) point)
+        Nothing -> False
+
+-- Converts a 2D point to a 1D index
+pointToIndex :: Int -> SingularPoint -> Int
+pointToIndex width (x, y) = y * width + x
+
+moveLayer :: Char -> (Int,Int) -> World -> World
+moveLayer c (dx,dy) w = w {worldLayers = M.update (\pts -> Just $ pts `shift` (dx + width*dy)) c (worldLayers w)}
+  where width = worldWidth w
+
+setPoint :: Char -> (Int,Int) -> World -> World
+setPoint c (x,y) w = w {worldPoints = M.insert c (x,y) (worldPoints w)}
+
+isOverlappingLayers :: Char -> Char -> World -> Bool
+isOverlappingLayers c1 c2 w
+    = fromMaybe False $ do
+        points1 <- M.lookup c1 (worldLayers w)
+        points2 <- M.lookup c2 (worldLayers w)
+        
+        return $ points1 `isOverlapping` points2
 
 
+-- isOverlapping n n
+-- time:  O(n) (approx.) [0 < n < 10000000000, 0 < secs < 7.97]
+-- space: O(n) (approx.) [0 < n < 10000000000, 0 < bytes < 30000141704]
+-- https://nux-pc/svn/Nux-SVN/My Programming/Scratchings/Advent of Code 2022/day17 (bits).hs/?r=1569
+isOverlapping :: Points -> Points -> Bool
+isOverlapping ps1 ps2 = (ps1 .&. ps2) /= zeroBits
 
 -- data Obj = Obj {
     -- objPoints  :: !Points,
