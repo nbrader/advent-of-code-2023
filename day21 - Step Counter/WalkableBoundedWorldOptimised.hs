63a0c0ca7e2856c20a65c911925c7d3f54a6e1ff
nbrader <nbrader+github@gmail.com>
2023-12-31 17:12:09 +0000

    Progressing new optimised world data type (WIP)

--- day21 - Step Counter/WalkableBoundedWorldOptimised.hs
+++ day21 - Step Counter/WalkableBoundedWorldOptimised.hs
@@ -7,20 +7,30 @@ module WalkableBoundedWorldOptimised (WalkableBoundedWorldOptimised(WalkableBoun
 -- Imports --
 -------------
 import Data.List (findIndex)
-import qualified Data.Map as M
+import qualified Data.Set as S
 import Data.Maybe (fromJust)
 import Data.Ord
 import Data.Bits
+import Control.Monad (guard)
 
 import Layer ( allDirs )
 
-import World as W ( World(..)
-                  , readWorld
-                  , showWorld
-                  , combineWorlds
-                  , moveLayerInWorld
-                  , cutLayerWithLayer
-                  , insertLayerAtPoint )
+import Layer ( SingularPoint
+             , Layer
+             , pointToIndex
+             , pointToLayer
+             , moveLayer
+             , movePoint
+             , isOverlapping
+             , diff
+             , up
+             , dn
+             , lt
+             , rt
+             , allDirs )
+
+import World as W
+            ( World )
 
 import WalkableWorld as Class
 
@@ -28,12 +38,47 @@ import WalkableBoundedWorld as B
                             ( charOrder
                             , addRocksToRightAndTop )
 
-newtype WalkableBoundedWorldOptimised = WalkableBoundedWorldOptimised {asWorld :: World}
+data WalkableBoundedWorldOptimised
+    = WalkableBoundedWorldOptimised
+            { worldBGChar :: Char
+            , worldWalls :: S.Set (Int,Int)
+            , worldPrevVisited :: S.Set (Int,Int)
+            , worldPrevPrevVisited :: S.Set (Int,Int)
+            , worldCountedOddSteps :: Integer
+            , worldCountedEvenSteps :: Integer
+            , worldWidth :: Int } deriving (Show)
 
 instance WalkableWorld WalkableBoundedWorldOptimised where
     -- Assumes all rows have equal length
-    readWorld :: String -> (Int, WalkableBoundedWorldOptimised)
-    readWorld = undefined -- fmap WalkableBoundedWorldOptimised . W.readWorld '.' ['S'] . addRocksToRightAndTop
+    readWorld :: String -> (Int,WalkableBoundedWorldOptimised)
+    readWorld inStr
+        = ( height
+          , WalkableBoundedWorldOptimised
+                { worldBGChar = bgChar
+                , worldWalls = undefined
+                , worldPrevVisited = undefined
+                , worldPrevPrevVisited = undefined
+                , worldCountedOddSteps = undefined
+                , worldCountedEvenSteps = undefined
+                , worldWidth = width } )
+      
+      where rows = lines inStr
+            height = length rows
+            width
+              | height == 0 = 0
+              | otherwise   = length $ head rows
+            char2Ds = readChar2DsFromRows rows
+            
+            readChar2DsFromRows :: [String] -> [(Char, (Int,Int))]
+            readChar2DsFromRows rows = do
+                (y',row) <- zip [0..] rows
+                (x,char) <- zip [0..] row
+                
+                let y = height - 1 - y'
+                
+                guard $ char /= bgChar
+                
+                return (char, (x, y))
 
     showWorld :: Int -> WalkableBoundedWorldOptimised -> String
     showWorld height w = undefined -- W.showWorld height charOrder (Class.asWorld w)
@@ -52,3 +97,7 @@ instance WalkableWorld WalkableBoundedWorldOptimised where
     
     oCount :: WalkableBoundedWorldOptimised -> Int
     oCount = undefined -- popCount . fromJust . M.lookup 'O' . worldLayers . Class.asWorld
+
+bgChar = '.'
+wallChar = '#'
+recentVisitChar = 'O'
